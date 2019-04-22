f2a05166d0200a88e09a6ddf5c0513a7a7066fc6
Chevonie Daniel <chevonie.daniel@mnsu.edu>
2019-04-22 18:35:58 +0000

    edit to Application Controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 class PagesController < ApplicationController
     def about
-        @title = 'About Us'
-        @content = 'Our mission is to provide students with easy access to an affordable Rental and Subleasing experience.'
+        
     end
     
     def rental_options
